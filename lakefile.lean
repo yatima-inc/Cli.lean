b5155d1791fef9594c86035862cec786d14c78f2
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-01-11 00:46:25 +0000

    bump YatimaStdLib

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,4 @@ require std from git
   "https://github.com/leanprover/std4/" @ "fde95b16907bf38ea3f310af406868fc6bcf48d1"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "cf2da463e263951019cb96e74d5fa6e51636e553"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "52d70775731234886aebd2852199ab39f395d6ac"
