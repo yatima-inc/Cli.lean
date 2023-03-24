4edcbffe84645067951d00fb4506466aa6b8d054
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-24 11:09:35 -0300

    update deps org

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,4 @@ require std from git
   "https://github.com/leanprover/std4/" @ "fde95b16907bf38ea3f310af406868fc6bcf48d1"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "649368d593f292227ab39b9fd08f6a448770dca8"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "649368d593f292227ab39b9fd08f6a448770dca8"
